96b059bf4ffa21be184a0a4c6bc712082ac1f7bf
Sofiia Sliusar <sofsliusar@gmail.com>
2025-03-17 23:05:34 +0200

    Refactor templates to DTL. Add navigation between pages.

--- musicapp/music/urls.py
+++ musicapp/music/urls.py
@@ -3,4 +3,12 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name="about"),
+    path('artists/', views.ArtistsView.as_view(), name="artists"),
+    path('albums/', views.AlbumView.as_view(), name="albums"),
+    path('playlist/', views.PlaylistView.as_view(), name="playlist"),
+    path('premium/', views.PremiumView.as_view(), name="premium"),
+    path('settings/', views.SettingsView.as_view(), name="settings"),
+    path('office/', views.OfficeView.as_view(), name="office"),
+
 ]
--- musicapp/music/views.py
+++ musicapp/music/views.py
@@ -6,3 +6,31 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'Home.html'
+
+
+class ArtistsView(TemplateView):
+    template_name = 'Artists.html'
+
+
+class AlbumView(TemplateView):
+    template_name = 'Albums.html'
+
+
+class PlaylistView(TemplateView):
+    template_name = 'Playlist.html'
+
+
+class PremiumView(TemplateView):
+    template_name = 'Premium.html'
+
+
+class SettingsView(TemplateView):
+    template_name = 'Settings.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'AboutUs.html'
+
+
+class OfficeView(TemplateView):
+    template_name = 'Office.html'
