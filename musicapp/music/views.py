a65bb4cddcbdcc7c867433e9720667b6ab6130c4
Sofiia Sliusar <sofsliusar@gmail.com>
2025-04-27 01:30:49 +0300

    Fix BUG with heart updatng after reload (same icons were the problem)

--- musicapp/music/views.py
+++ musicapp/music/views.py
@@ -1067,9 +1067,12 @@ class AddToFavoritesView(View):
 
             # Return the filled heart SVG icon when added
             icon_svg = '''
-            <svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="#e600ff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-            </svg>
+            <svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#e600ff">
+                              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5
+                                       2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09
+                                       C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5
+                                       c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+                            </svg>
             '''
             return JsonResponse({'status': 'added', 'icon': icon_svg})
 
@@ -1079,7 +1082,10 @@ class AddToFavoritesView(View):
             playlist_song.delete()
             icon_svg = '''
             <svg class="heart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="#e600ff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-            </svg>
+                              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5
+                                       2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09
+                                       C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5
+                                       c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+                            </svg>
             '''
             return JsonResponse({'status': 'already_added', 'icon': icon_svg})
